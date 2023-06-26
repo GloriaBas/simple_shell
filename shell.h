03723c9f363768556acb14eb288a3b6deb223ef3
GloriaBas <gloudos@yahoo.com>
2023-06-26 16:28:09 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -6,6 +6,18 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+/* str_modifiers */
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *str);
+int _puts(char *str);
+char *read_line(void);
 
 
 
