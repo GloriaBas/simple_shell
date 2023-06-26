6b3f06cc97839cb33528b51737b4127ed9e457d1
GloriaBas <gloudos@yahoo.com>
2023-06-26 06:13:08 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <process.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+#endif
