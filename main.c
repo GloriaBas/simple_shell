bfef1a11c138d07a0c0ff01b43c78de17fa67748
GloriaBas <gloudos@yahoo.com>
2023-06-28 11:23:16 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * main - A function to run the shell
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char **argv)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (argc == 2)
+	{
+		fd = open(av[1], 0_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(argc[0]);
+				_eputs(": 0; Cant open ");
+				_eputs(argv[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, argv);
+	return (EXIT_SUCCESS);
+}
